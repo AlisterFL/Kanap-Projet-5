0f1ec7732f6f39eeb58cbeca62c71a2199016069
AlisterFL <123501329+AlisterFL@users.noreply.github.com>
2023-04-11 08:44:11 +0200

    correction de problème + organisation du code

--- front/js/productPage.js
+++ front/js/productPage.js
@@ -1,25 +1,77 @@
-// Récupérez l'URL de la page
-const currentUrl = window.location.href;
+import { getDataFromUrl, store } from "./tools.js";
 
-// Créez un nouvel objet URL à partir de l'URL actuelle
-const url = new URL(currentUrl);
+const id = getDataFromUrl("id");
+let article = await getProduct(id);
 
-// Utilisez URLSearchParams pour extraire la valeur de l'ID
-const params = new URLSearchParams(url.search);
-const id = params.get("id");
+display(article);
+listenForCartAddition(article);
 
-console.log(id);
+function listenForCartAddition(article) {
+  //Action lors de l'ajout au panier
+  const addToCart = document.querySelector("#addToCart");
+  let cart = [];
 
-//Récuperation des informations du produits
-let article = {};
+  addToCart.addEventListener("click", () => {
+    const color = document.querySelector("#colors").value;
+    const quantity = document.querySelector("#quantity").value;
+    const id = article._id;
 
-async function getProduct(id) {
-  const response = await fetch(`http://localhost:3000/api/products/${id}`);
-  article = await response.json();
-  console.log(article);
+    //Si toutes les infos ne sont pas remplies
+    if (color.length === 0) {
+      alert("Veuillez choisir une couleur.");
+      return;
+    }
+    if (quantity < 0 || quantity > 100) {
+      alert("Veuillez choisir une quantité acceptée");
+      return;
+    }
+
+    const newProduct = { id, color, quantity };
+
+    // Récupérer le contenu actuel du panier dans le localStorage
+    const cartJSON = localStorage.getItem("cart");
+    let cart = [];
+
+    if (cartJSON != null) {
+      cart = JSON.parse(cartJSON);
+    }
+
+    // Vérifier si l'article existe déjà dans le panier
+    let productExists = false;
+
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].id === newProduct.id && cart[i].color === newProduct.color) {
+        cart[i].quantity = parseInt(cart[i].quantity);
+        newProduct.quantity = parseInt(newProduct.quantity);
+        cart[i].quantity += newProduct.quantity;
+        if (Number(cart[i].quantity) > 100) {
+          console.log(cart[i].quantity);
+          console.log(newProduct.quantity);
+          alert(
+            "Vous ne pouvez pas avoir plus de 100 même article dans votre panier."
+          );
+          return;
+        }
+        productExists = true;
+        break;
+      }
+    }
+
+    // S'il n'existe pas encore, ajouter au panier
+    if (!productExists) {
+      cart.push(newProduct);
+    }
+    console.log(cart);
+
+    store("cart", cart);
+    alert(
+      "Votre produit a bien été ajouté ! \n Vous allez être redirigé vers l'accueil"
+    );
+    window.location.href = "index.html";
+  });
 }
 
-getProduct(id).then(() => {
+function display(article) {
   //Ajout du nom de l'article en titre de page
   const titlePage = document.querySelector("title");
   titlePage.innerText = article.name;
@@ -47,56 +99,9 @@ getProduct(id).then(() => {
     colorOption.textContent = color;
     selectColor.appendChild(colorOption);
   });
-});
+}
 
-//Action lors de l'ajout au panier
-const addToCart = document.querySelector("#addToCart");
-
-addToCart.addEventListener("click", () => {
-  const selectColor = document.querySelector("#colors");
-  const selectNumber = document.querySelector("#quantity");
-
-  const id = article._id;
-  const color = selectColor.value;
-  const number = selectNumber.value;
-
-  //Si toutes les infos ne sont pas remplies
-  if (color == null || number == 0) {
-    alert("Veuillez remplir tous les champs nécessaires");
-    return;
-  }
-
-  const newProduct = { id, color, number };
-
-  // Récupérer le contenu actuel du panier dans le localStorage
-  const cartJSON = localStorage.getItem("cart");
-  let cart = [];
-
-  if (cartJSON != null) {
-    cart = JSON.parse(cartJSON);
-  }
-
-  // Vérifier si l'article existe déjà dans le panier
-  let productExists = false;
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].id === newProduct.id && cart[i].color === newProduct.color) {
-      cart[i].number = parseInt(cart[i].number);
-      newProduct.number = parseInt(newProduct.number);
-      cart[i].number += newProduct.number;
-      productExists = true;
-      break;
-    }
-  }
-
-  // S'il n'existe pas encore, ajouter au panier
-  if (!productExists) {
-    cart.push(newProduct);
-  }
-  console.log(cart);
-
-  // Convertir le tableau en chaîne JSON
-  const updatedCartJSON = JSON.stringify(cart);
-
-  // Enregistrer le tableau mis à jour dans le localStorage
-  localStorage.setItem("cart", updatedCartJSON);
-});
+async function getProduct(id) {
+  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+  return await response.json();
+}
---
+++ front/js/tools.js
@@ -0,0 +1,19 @@
+function getDataFromUrl(key) {
+  // Récupérez l'URL de la page
+  const currentUrl = window.location.href;
+
+  // Créez un nouvel objet URL à partir de l'URL actuelle
+  const url = new URL(currentUrl);
+
+  // Utilisez URLSearchParams pour extraire la valeur de l'ID
+  const params = new URLSearchParams(url.search);
+
+  return params.get(key);
+}
+
+function store(key, value) {
+  //upload le local storage, directement en JSON
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export { getDataFromUrl, store };
